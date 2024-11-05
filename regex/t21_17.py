4ded19201a3273a4878bf908e4fa632b9d363324
Alex Katrenko <112182502+codemorphist@users.noreply.github.com>
2024-11-05 17:07:22 +0200

    Completed Task T21.17

---
+++ regex/t21_17.py
@@ -0,0 +1,59 @@
+import re
+
+
+RESTRICTED = {
+    "neflix.com",
+    "vk.com",
+    "vk.ru",
+    "ok.ru",
+    "onlyfans.com",
+    "rezka.hd",
+    "kinogo.biz",
+    "youtube.com",
+    "telegram.com"
+}
+
+P_COMP = r"((\d{1,3}\.){3}\d{1,3})"
+P_ADDR = r"https?:\/\/([a-zA-Z0-9.-]+)"
+P_TIME = r"((\d{2}\.\d{2}\.\d{4})\s*(\d{2}:\d{2}:\d{2}))"
+
+p_com = re.compile(P_COMP)
+p_addr = re.compile(P_ADDR)
+p_time = re.compile(P_TIME)
+
+
+def sanitize_url(url: str) -> str:
+    return url.replace("www.", "")
+
+
+def process_line(line: str) -> tuple:
+    comp = re.search(p_com, line).groups()[0]
+    addr = re.search(p_addr, line).groups()[0]
+    time = re.search(p_time, line).groups()[0]
+
+    addr = sanitize_url(addr)
+
+    return (comp, addr, time)
+
+
+def process_file(path: str, out: str):
+    file = ""
+    res_records = []
+    with open(path, "r") as f:
+        for line in f.readlines():
+            rec = process_line(line)
+
+            if rec[1] in RESTRICTED:
+                res_records.append(rec)
+
+    with open(out, "w") as f:
+        f.write(
+            "\n".join([
+                " ".join(rec) for rec in res_records
+            ])
+        )
+
+
+
+if __name__ == "__main__":
+    process_file("./provider_data.txt", "porushniki.txt")
---
+++ regex/t21_17_gen.py
@@ -0,0 +1,48 @@
+from datetime import datetime, timedelta
+import random
+
+
+def get_comp_addr() -> str:
+    return ".".join([str(random.randint(0, 255)) for _ in range(4)]) 
+
+
+
+SITES = ["google.com", "netflix.com", "vk.com", "vk.ru", "ok.ru",
+         "onlyfans.com", "rezka.hd", "kinogo.biz", "math.ru",
+         "duckduckgo.com", "github.com", "youtube.com", "reddit.com",
+         "diia.gov.ua", "wikipedia.com", "telegram.com", "dou.ua",
+         "pentagon.com", "mil.ua", "privat24.ua"]
+def get_site() -> str:
+    site = random.choice(SITES) 
+    protocol = random.choice(["https://", "http://"])
+
+    return f"{protocol}www.{site}{random.choice(['/', ''])}"
+
+
+def get_time() -> str:
+    startdate = datetime(2022, 1, 1)
+    delta = timedelta(days = random.randint(0, 365 * 2), 
+                      hours = random.randint(0, 24), 
+                      minutes = random.randint(0, 60), 
+                      seconds = random.randint(0, 60))
+    gen_date = startdate + delta
+    return gen_date.strftime("%d.%m.%Y %H:%M:%S")
+
+def get_record() -> str:
+    return " ".join([
+        get_comp_addr(),
+        get_site(),
+        get_time()
+    ])
+
+
+def gen_records_list(count: int, out: str):
+    with open(out, "w") as f:
+        f.write(
+            "\n".join([get_record() for _ in range(count)])
+        )
+        
+
+if __name__ == "__main__":
+    gen_records_list(10000, "provider_data.txt")
+
